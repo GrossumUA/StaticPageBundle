7a777156a4f0d866546d783e3d8e362a30346c00
Алексей Черноиван <achernoivan@grossum.com>
2016-02-19 13:39:56 +0200

    code review fixes

--- Entity/EntityManager/StaticPageManager.php
+++ Entity/EntityManager/StaticPageManager.php
@@ -11,23 +11,39 @@ class StaticPageManager
     use SaveUpdateInManagerTrait;
 
     /**
+     * @var string
+     */
+    private $staticPageClass;
+
+    /**
      * @var ObjectManager
      */
     private $objectManager;
 
     /**
-     * @param ObjectManager $objectManager
+     * @var StaticPageRepository
      */
-    public function __construct(ObjectManager $objectManager)
+    private $repository;
+
+    /**
+     * @param ObjectManager $objectManager
+     * @param string $staticPageClass
+     */
+    public function __construct(ObjectManager $objectManager, $staticPageClass)
     {
-        $this->objectManager = $objectManager;
+        $this->objectManager   = $objectManager;
+        $this->staticPageClass = $staticPageClass;
     }
 
     /**
      * @return StaticPageRepository
      */
-    private function getRepository()
+    public function getRepository()
     {
-        return $this->objectManager->getRepository('GrossumStaticPageBundle:StaticPage');
+        if ($this->repository === null) {
+            $this->repository = $this->objectManager->getRepository($this->staticPageClass);
+        }
+
+        return $this->repository;
     }
 }
